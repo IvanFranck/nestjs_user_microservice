ada4bbae8f45a4b7085a135b74087f8b80f82479
ivan nzima <nzimaivan007@gmail.com>
2023-05-13 10:48:13 +0100

    added code for pass last test

--- src/users/__mocks__/users.service.ts
+++ src/users/__mocks__/users.service.ts
@@ -2,4 +2,5 @@ import { userStub } from '../test/stubs/user.stub';
 
 export const UsersService = jest.fn().mockReturnValue({
   createUser: jest.fn().mockResolvedValue(userStub()),
+  findUser: jest.fn().mockResolvedValue(userStub()),
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { User } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
@@ -10,5 +10,10 @@ export class UsersController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Get(':id')
+  async findUser(@Param('id') id: string): Promise<User> {
+    return this.userService.findUser(id);
   }
 }
